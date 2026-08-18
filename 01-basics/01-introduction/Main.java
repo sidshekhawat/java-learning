21950c8dbe382d721f73a8a479f40486cf4af1d3
Siddhant Singh <sidshekhawat16@gmail.com>
2026-08-18 18:34:31 +0530

    feat: complete introduction to Java

---
+++ 01-basics/01-introduction/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    // THIS IS MY FIRST JAVA PROGRAM
+
+    public static void main(String[] args){
+
+        System.out.println("I like pizza!");
+        System.out.println("& it is really good!");
+        System.out.print("Buy me pizza!");
+    }
+}
